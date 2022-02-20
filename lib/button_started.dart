c0b361b233b99510332fe28eebb9e4283a544fea
Gavin101005 <m.gavin.aga10@gmail.com>
2022-02-20 19:46:00 +0700

    Intrp_Slider

--- lib/button_started.dart
+++ lib/button_started.dart
@@ -10,37 +10,21 @@ class HeaderButton extends StatefulWidget {
 class _HeaderButtonState extends State<HeaderButton> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          "Welcome to Kievit",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23.0),
+    return MaterialButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        minWidth: 250.0,
+        height: 50.0,
+        color: Colors.red,
+        textColor: Colors.white,
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (_) => HomePage()),
+          );
+        },
+        child: Text(
+          'Get Started',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
         ),
-        SizedBox(
-          height: 70,
-        ),
-        Text("Deskripsi", style: TextStyle(fontSize: 19.0)),
-        SizedBox(
-          height: 30,
-        ),
-        MaterialButton(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-          minWidth: 250.0,
-          height: 50.0,
-          color: Colors.red,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(builder: (_) => HomePage()),
-            );
-          },
-          child: Text(
-            'Get Started',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-          ),
-        ),
-      ],
-    );
+      );
   }
 }
